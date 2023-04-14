c351acf619b3662be3eaecf579e7c678dce87562
Mohd Saood Akhtar <48763014+saood011@users.noreply.github.com>
2023-04-14 15:54:30 +0200

    Initial Commit

---
+++ AstroHunt/AstroHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroHuntApp.swift
+//  AstroHunt
+//
+//  Created by saood.akhtar on 14.04.23.
+//
+
+import SwiftUI
+
+@main
+struct AstroHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
