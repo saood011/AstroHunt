6a5933f3d447c1d88b3c580e3e85297c99ff02eb
Mohd Saood Akhtar <48763014+saood011@users.noreply.github.com>
2023-05-11 13:09:44 +0200

    Image code refactor as loggedInProfileImage

--- AstroHunt/Views/AstronautList.swift
+++ AstroHunt/Views/AstronautList.swift
@@ -26,36 +26,18 @@ struct AstronautList: View {
             .navigationTitle("All Astronauts")
             .toolbar{
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    HStack{
-          
-                
-                    }
                     Button(action: {
-                        showingSheet.toggle()
-                    }, label: {
-                        LazyImage(url:  URL(string: loginManager.dpUrl)) { state in
-                            if let image = state.image {
-                                image.resizable().aspectRatio(contentMode: .fill)
-                            } else if state.error != nil {
-                                Color.red // Indicates an error
-                            } else {
-                                Color.blue // Acts as a placeholder
-                            }
-                        }
-                        .clipShape(Circle()).frame(width: 40, height: 40)
-                        .overlay {
-                            Circle().stroke(.white, lineWidth: 2)
-                        }
-                        .cornerRadius(8)
-                        .shadow(radius: 2)
-                    })
+                            showingSheet.toggle()
+                        }, label: {
+                            LoggedInProfileImage()
+                        })
                         .sheet(isPresented: $showingSheet) {
                             SheetView().presentationDetents([.height(200)])
                         }
                 }
             }
             .listStyle(.inset)
-                }
+        }
         .onAppear {
             network.downloadApiRespnose()
         }
